07f2f78e3a439194bfaa29bfe00ccf066ce8a537
gardyna <dagur13@ru,is>
2015-05-04 14:01:12 +0000

    Main Menu final?
    potentialy fully completed start menu

--- DuoDerby/Assets/Scripts/UIManager.cs
+++ DuoDerby/Assets/Scripts/UIManager.cs
@@ -2,7 +2,24 @@
 using System.Collections;
 
 public class UIManager : MonoBehaviour {
+	public Animator startButton;
+	public Animator settingsButton;
+	public Animator dialog;
+
 	public void StartGame() {
 		Application.LoadLevel("TestScene");
+	}
+
+	public void OpenSettings() {
+		startButton.SetBool("isHidden", true);
+		settingsButton.SetBool("isHidden", true);
+		dialog.enabled = true;
+		dialog.SetBool("isHidden", false);
+	}
+
+	public void CloseSettings() {
+		startButton.SetBool("isHidden", false);
+		settingsButton.SetBool("isHidden", false);
+		dialog.SetBool("isHidden", true);
 	}
 }
