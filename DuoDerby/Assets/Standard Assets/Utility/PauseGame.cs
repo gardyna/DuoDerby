ff4130c6dcb1b2c3a64587e8763947b3135da038
kjartank13 <kjartank13@ru.is>
2015-05-06 10:03:18 +0000

    Scene with wider roads

---
+++ DuoDerby/Assets/Standard Assets/Utility/PauseGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("p")){
+			if (Time.timeScale == 1)
+				Time.timeScale =0;
+			else
+				Time.timeScale =1;
+		}
+	}
+}
