4f2f192b5bf227724c25d3a8937d58cc545f036f
krissikrull <kristinnts11@ru.is>
2015-05-07 15:16:03 +0000

    b

--- DuoDerby/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ DuoDerby/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -4,34 +4,46 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
-    [RequireComponent(typeof (CarController))]
-    public class CarUserControl : MonoBehaviour
-    {
-        private CarController m_Car; // the car controller we want to use
+	[RequireComponent(typeof (CarController))]
+	public class CarUserControl : MonoBehaviour
+	{
+		private CarController m_Car; // the car controller we want to use
 		public int driver;
 		public int engine;
-
-
-        private void Awake()
-        {
-            // get the car controller
-            m_Car = GetComponent<CarController>();
-        }
-
-
-        private void FixedUpdate()
-        {
-            // pass the input to the car!
+		
+		
+		private void Awake()
+		{
+			// get the car controller
+			m_Car = GetComponent<CarController>();
+		}
+		
+		
+		private void FixedUpdate()
+		{
+			// pass the input to the car!
 			float h = CrossPlatformInputManager.GetAxis("Horizontal_p" + driver.ToString());
-            float v = CrossPlatformInputManager.GetAxis("Vertical_p" + engine.ToString());
-#if !MOBILE_INPUT
-            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
-#else
-            m_Car.Move(h, v, v, 0f);
-#endif
-        }
-
+			float v = CrossPlatformInputManager.GetAxis("Vertical_p" + engine.ToString());
+			if(Input.GetKeyDown(KeyCode.W))
+			{
+				h = CrossPlatformInputManager.GetAxis("Horizontal_p" + driver.ToString());
+			}
+			if(Input.GetKeyDown(KeyCode.A))
+			{
+				v = CrossPlatformInputManager.GetAxis("Vertical" + driver.ToString());
+			}
+			if(Input.GetKeyDown(KeyCode.D))
+			{
+				v = CrossPlatformInputManager.GetAxis("Vertical" + driver.ToString());
+			}
+			#if !MOBILE_INPUT
+			//float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+			m_Car.Move(h, v, v, 0f);
+			#else
+			m_Car.Move(h, v, v, 0f);
+			#endif
+		}
+		
 		public void Switch() {
 			Debug.Log("Switching");
 			if (driver == 1) {
@@ -42,5 +54,5 @@ namespace UnityStandardAssets.Vehicles.Car
 				engine = 2;
 			}
 		}
-    }
+	}
 }
