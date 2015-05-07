5494f74c7359cbba799145a0d969c28e551fd81e
gardyna <dagur13@ru,is>
2015-05-07 14:08:18 +0000

    switch pickup with animations

--- DuoDerby/Assets/Scripts/SwitchPickup.cs
+++ DuoDerby/Assets/Scripts/SwitchPickup.cs
@@ -3,22 +3,21 @@ using System.Collections;
 using UnityStandardAssets.Vehicles.Car;
 
 public class SwitchPickup : MonoBehaviour {
-	public GameObject Player;
+	public Animator anim;
 	private bool working = true;
 
 	IEnumerator OnTriggerEnter(Collider other) {
-		Debug.Log(other.gameObject.tag);
 		if (other.gameObject.tag == "Player") {
 			if (working) {
-				Debug.Log("Enter!!!");
 				working = false;
 				Renderer rend = GetComponent<Renderer>();
 				rend.enabled = false;
+				anim.SetBool("Play", true);
 				other.gameObject.GetComponentInParent<CarUserControl>().Switch();
 				yield return new WaitForSeconds(10);
+				anim.SetBool("Play", false);
 				rend.enabled = true;
 				working = true;
-				Debug.Log("Done");
 			}
 			
 		}
