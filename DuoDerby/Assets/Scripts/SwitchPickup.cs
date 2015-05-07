f90298031d7193a9cc15acc305e4068fc8a59f98
gardyna <dagur13@ru,is>
2015-05-07 12:41:58 +0000

    pickup render changes not wotking but otherwise fine

---
+++ DuoDerby/Assets/Scripts/SwitchPickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+
+public class SwitchPickup : MonoBehaviour {
+	public GameObject Player;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log(other.gameObject.tag);
+		if (other.gameObject.tag == "Player") {
+			Debug.Log("Enter!!!");
+			other.gameObject.GetComponentInParent<CarUserControl>().Switch();
+			this.GetComponent<Renderer>().enabled = false;
+			StartCoroutine(Wait());
+			this.GetComponent<Renderer>().enabled = false;
+		}
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds(2);
+	}
+}
