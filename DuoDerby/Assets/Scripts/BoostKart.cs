3fca3ce4ceb02723c6a3976696122695cf8138a6
kjartank13 <kjartank13@ru.is>
2015-05-07 14:24:26 +0000

    Boost pad

---
+++ DuoDerby/Assets/Scripts/BoostKart.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoostKart : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody rb;
+	private bool launched;
+	private int count;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		launched = false;
+		count = 0;
+	}
+
+	void Update() {
+		if (launched && (count > 0)) {
+			count = count - 1;
+			rb.AddForce (transform.forward * thrust);
+		} 
+		else if (launched && (count <= 0)) {
+			launched = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+//		Debug.Log ("Entered object");
+		if (other.gameObject.CompareTag ("Boost")) {
+			launched = true;
+			count = 10;
+//			Debug.Log("Triggered");
+		}
+
+	}
+}
