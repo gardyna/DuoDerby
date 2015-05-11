6612403607fd19647e1163631d56b61162735ae3
kjartank13 <kjartank13@ru.is>
2015-05-11 14:22:37 +0000

    Made HowTo page

---
+++ DuoDerby/Assets/Scripts/HowTo_return.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowTo_return : MonoBehaviour {
+	public string menu;
+	
+	public void MenuReturn() {
+		Application.LoadLevel(menu);
+	}
+}
