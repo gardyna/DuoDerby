621d4e65966c3b2a14942eab8a90a64fe7137a9f
valgeir <valgeir13@ru.is>
2015-05-13 15:05:36 +0000

    Added ValliTest enviro to prefab

--- DuoDerby/Assets/Scripts/SwitchPickup.cs
+++ DuoDerby/Assets/Scripts/SwitchPickup.cs
@@ -1,24 +1,26 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.Vehicles.Car;
 
 public class SwitchPickup : MonoBehaviour {
 	public Animator anim;
 	private bool working = true;
+	public float high;
+	public float low;
 
 	IEnumerator OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
 			if (working) {
 				working = false;
 				float spawnDelay;
-				Renderer rend = GetComponent<Renderer>();
-				rend.enabled = false;
+				//Renderer rend = GetComponent<Renderer>();
+				//rend.enabled = false;
 				anim.SetBool("Play", true);
 				other.gameObject.GetComponentInParent<CarUserControl>().Switch();
-				spawnDelay = Random.Range(5f,30f);
+				spawnDelay = Random.Range(low,high);
 				yield return new WaitForSeconds(spawnDelay);
 				anim.SetBool("Play", false);
-				rend.enabled = true;
+				//rend.enabled = true;
 				working = true;
 			}
 			
